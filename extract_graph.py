ed73205a09f710bb53ed0f6c8ccca67afdb7bd6e
tianyudl <justinxu.aiesechit@gmail.com>
2018-04-17 01:19:10 +0000

    add small london graph

--- extract_graph.py
+++ extract_graph.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-nodes = 89127
+nodes = 36545
 #matrix = [[0 for x in range(nodes)] for y in range(nodes)]
 
 matrix = np.zeros((nodes,nodes))
 print(matrix.shape)
-with open("../data/London.pypgr", "r") as f:
+with open("data/smallLondon.pypgr", "r") as f:
     for line in f:
         x = line.split(" ")
         if len(x) >= 6 :
             matrix[int(x[0])][int(x[1])] = float(x[2])
-            print line
+            print(line)
 
 file = open("data/smallLondonGraph.txt", "w")
 for x in range(nodes):
     for y in range(nodes):
         if matrix[x][y] > 0:
             file.write(str(x)+" "+str(y)+" "+str(matrix[x][y])+"\n")
+    print(str(x)+"th completed")
 file.close()
     
